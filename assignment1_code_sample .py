6e9f2b1da02dfbe2be1d0cb08efb063222c195cc
fatema turaba <fturaba@rrc.ca>
2025-02-25 10:04:45 -0600

    updated the files based on given instruction.

--- assignment1_code_sample .py
+++ assignment1_code_sample .py
@@ -3,9 +3,11 @@ Module: assignment1_code_sample
 This module provides functions to get user input, fetch data from an API, store data in a database,
 and send an email notification.
 """
-import os
-import pymysql
+
 from urllib.request import urlopen
+import pymysql
+import os
+
 
 db_config = {"host": "mydatabase.com", "user": "admin", "password": "secret123"}
 
@@ -14,8 +16,8 @@ def get_user_input():
     """
     Prompt the user for their name and return the input.
     """
-    user_input = input("Enter your name: ")
-    return user_input
+    name_input = input("Enter your name: ")
+    return name_input
 
 
 def send_email(to, subject, body):
@@ -30,8 +32,8 @@ def get_data():
     Fetch data from an API endpoint and return the decoded response.
     """
     url = "http://insecure-api.com/get-data"
-    data = urlopen(url).read().decode()
-    return data
+    api_data = urlopen(url).read().decode()
+    return api_data
 
 
 def save_to_db(data):
@@ -49,6 +51,6 @@ def save_to_db(data):
 
 if __name__ == "__main__":
     user_input = get_user_input()
-    data = get_data()
-    save_to_db(data)
+    retrieved_data = get_data()
+    save_to_db(retrieved_data)
     send_email("admin@example.com", "User Input", user_input)
